6e916d0197e488f09ffe2188c279f8067a523c18
Pappyjay23 <pjinadu23@gmail.com>
2022-03-13 23:44:39 +0100

    Added responsiveness to the country links for each tab

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -62,12 +62,18 @@ const TopLink = styled(Link)`
 
 const CountryLinks = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     column-gap: 1rem;
     row-gap: 1rem;
     width: 95%;
     margin: 0 auto;
     margin-bottom: 3rem;
+    @media ${devices.mobileL} {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media ${devices.mobileS} {
+        grid-template-columns: 1fr;
+    }
 `
 const CountryLink = styled(Link)`
     text-decoration: none;
